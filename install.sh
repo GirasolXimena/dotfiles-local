9450d41fca60f0a19390b1f151487d1d746bc340
Robert Andrade <srandradejr@gmail.com>
2019-08-05 13:19:57 -0500

    Check if brew is installed before running brewfile

--- install.sh
+++ install.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
 # Clone dotfiles from thoughtbot
-echo "\nInstalling thoughtbot dotfiles\n"
+echo `Installing thoughtbot dotfiles`
 git clone https://github.com/thoughtbot/dotfiles.git ~/dotfiles
 
+# Check if brew is installed, if it is then run bundle install and if it is not then install it
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    ruby -e `$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)`
+else
+      # Update Homebrew
+    brew update
+fi
+
+#Install apps from brew bundle
+brew bundle install
+
 # Install dotfiles and run rcup
-echo "\nInstalling and running dotfiles\n"
+echo `Installing and running dotfiles`
 env RCRC=$HOME/dotfiles/rcrc rcup
 
 # Change from vim to neovim
-echo "\nInstalling and running vim packages on neovim\n"
+echo `Installing and running vim packages on neovim`
 touch ~/.config/nvim/init.vim
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
 source ~/.vimrc
 
-# Zsh as login shell 
+# Zsh as login shell
 chsh -s $(which zsh)
 
 # Finished installing message
-echo "\nFinished installing :-)\n"
+echo `Finished installing :-)`
