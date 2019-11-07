c9f4ab496a337a73906d7099d6c80489c6117445
Robert Andrade <srandradejr@gmail.com>
2019-11-07 15:48:53 -0700

    clean up install script

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
-#!/bin/bash
-
-# Clone dotfiles from thoughtbot
-echo `Installing thoughtbot dotfiles`
-git clone https://github.com/thoughtbot/dotfiles.git ~/dotfiles
-
+#!/bin/bash # Clone dotfiles from thoughtbot
+DOTFILES="~/dotfiles"
+if [ -f "$DOTFILES" ]; then
+  git clone https://github.com/thoughtbot/dotfiles.git ~/dotfiles
+  echo `Installing thoughtbot dotfiles`
+else
+  echo "dotfiles already installed, skipping..."
+fi
 # Check if brew is installed, if it is then run bundle install and if it is not then install it
 which -s brew
 if [[ $? != 0 ]] ; then
     # Install Homebrew
     ruby -e `$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)`
 else
-      # Update Homebrew
+	# Update Homebrew
     brew update
 fi
 
@@ -18,11 +20,11 @@ fi
 brew bundle install
 
 # Install dotfiles and run rcup
-echo `Installing and running dotfiles`
+echo "Installing and running dotfiles"
 env RCRC=$HOME/dotfiles/rcrc rcup
 
 # Change from vim to neovim
-echo `Installing and running vim packages on neovim`
+echo "Installing and running vim packages on neovim"
 touch ~/.config/nvim/init.vim
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
@@ -32,4 +34,4 @@ source ~/.vimrc
 chsh -s $(which zsh)
 
 # Finished installing message
-echo `Finished installing :-)`
+echo "Finished installing"
