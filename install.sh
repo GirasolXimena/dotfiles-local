49388b25e5fe13b74288c8f7cee050d6da874f79
Robert Andrade <srandradejr@gmail.com>
2019-11-07 16:59:44 -0700

    Cleaned up install script so it runs faster

--- install.sh
+++ install.sh
@@ -1,37 +1,49 @@
-#!/bin/bash # Clone dotfiles from thoughtbot
-DOTFILES="~/dotfiles"
+#!/bin/bash 
+
+# Clone dotfiles from thoughtbot
+DOTFILES="$HOME/dotfiles"
 if [ -f "$DOTFILES" ]; then
-  git clone https://github.com/thoughtbot/dotfiles.git ~/dotfiles
-  echo `Installing thoughtbot dotfiles`
+  git clone https://github.com/thoughtbot/dotfiles.git $HOME/dotfiles
+  echo "Installing thoughtbot dotfiles"
 else
   echo "dotfiles already installed, skipping..."
 fi
+
 # Check if brew is installed, if it is then run bundle install and if it is not then install it
 which -s brew
 if [[ $? != 0 ]] ; then
-    # Install Homebrew
-    ruby -e `$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)`
+	ruby -e `$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)`
 else
-	# Update Homebrew
-    brew update
+	brew update
 fi
 
 #Install apps from brew bundle
 brew bundle install
 
 # Install dotfiles and run rcup
-echo "Installing and running dotfiles"
 env RCRC=$HOME/dotfiles/rcrc rcup
+echo "Installing and running dotfiles config"
 
 # Change from vim to neovim
-echo "Installing and running vim packages on neovim"
-touch ~/.config/nvim/init.vim
-set runtimepath^=~/.vim runtimepath+=~/.vim/after
+
+NVIMFILE="$HOME/.config/nvim/init.vim"
+if [ ! -f "$NVIMFILE" ]; then
+	mkdir -p "$(dirname "$NVIMFILE")" && touch "$NVIMFILE"
+	echo "Making nvim init file"
+fi
+echo "Installing and running vim packages for neovim"
+echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
-source ~/.vimrc
+source ~/.vimrc" > $NVIMFILE
 
 # Zsh as login shell
-chsh -s $(which zsh)
+
+if [ "$(which zsh)" == "/usr/local/bin/zsh" ]; then
+	echo "Already have oh-my-zsh as login shell"
+else
+	chsh -s $(which zsh)
+fi
 
 # Finished installing message
+
 echo "Finished installing"
